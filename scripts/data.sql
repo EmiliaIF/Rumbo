6f36440b1d3a1d23248404b67fa87a1454220234
Jonatan Hallenberg <jonatan.hallenberg@sprinto.se>
2022-04-01 00:12:31 +0200

    refactor routes

--- scripts/data.sql
+++ scripts/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO employees
+INSERT INTO employees(email, firstname, lastname, fullname) VALUES('kalle.anka@gmail.com', 'Kalle', 'Anka', 'Kalle Anka');
+INSERT INTO employees(email, firstname, lastname, fullname) VALUES('kajsa.anka@gmail.com', 'Kajsa', 'Anka', 'Kajsa Anka');
+
+INSERT INTO projects(customer_name, project_name, agreement_ref, active, created_at) VALUES('Åkeriet', 'Hemsida akeriet.se', 'AK132', true, CURRENT_TIMESTAMP)
+
