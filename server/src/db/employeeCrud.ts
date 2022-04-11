929c46f153c096e360f2b7743dff2612efffd9e3
Qim Lönnegren <qim.lonnegren@iths.se>
2022-04-11 11:14:05 +0200

    Add getEmployee function

--- server/src/db/employeeCrud.ts
+++ server/src/db/employeeCrud.ts
@@ -5,3 +5,9 @@ export const createEmployee = async (employee: employeeType) => {
   await newEmployee.save();
   return newEmployee;
 };
+
+export const getEmployee = async (employee: employeeType) => {
+  const readEmployee = new employeeModel(employee);
+  await readEmployee.save();
+  return readEmployee;
+};
